998c557921303a26d77545b012648b387330c8f6
pawan <pawanbhatta178@gmail.com>
2021-03-18 21:25:04 -0400

    Project 1 done

--- project1.sh
+++ project1.sh
@@ -141,3 +141,48 @@ pawanbhatta@pawans-MBP CloudComputing % kubectl get nodes
 NAME                            STATUS   ROLES    AGE   VERSION
 ip-192-168-59-48.ec2.internal   Ready    <none>   50m   v1.17.12-eks-7684af
 ip-192-168-8-160.ec2.internal   Ready    <none>   50m   v1.17.12-eks-7684af
+
+#Deploying nginx web server in the cluster
+pawanbhatta@pawans-MBP CloudComputing % kubectl create deployment nginx-deployment --image=nginx
+
+#Editing deployment
+pawanbhatta@pawans-MBP CloudComputing % kubectl edit deployments
+
+#Getting all running containers
+pawanbhatta@pawans-MBP CloudComputing % kubectl get pods
+NAME                               READY   STATUS    RESTARTS   AGE
+nginx-deployment-ddd976fc7-n87hs   1/1     Running   0          14m
+
+#Opening the deployment yaml file
+pawanbhatta@pawans-MBP ~ % kubectl edit deployments
+
+#Deploying using yaml config file that has multiple instance of the web server
+pawanbhatta@pawans-MBP CloudComputing % kubectl apply -f nginx-deployment.yaml
+deployment.apps/nginx-deployment2 created
+
+#Viwing all container that are running
+pawanbhatta@pawans-MBP CloudComputing % kubectl get pods
+NAME                                 READY   STATUS    RESTARTS   AGE
+nginx-deployment2-574b87c764-4pwgh   1/1     Running   0          2m9s
+nginx-deployment2-574b87c764-mjxmw   1/1     Running   0          2m9s
+
+#Applying software update 
+pawanbhatta@pawans-MBP CloudComputing % kubectl apply -f nginx-deployment.yaml
+deployment.apps/nginx-deployment2 configured
+
+#Previous pods are now replaced with the new version 
+pawanbhatta@pawans-MBP CloudComputing % kubectl get pods
+NAME                                 READY   STATUS    RESTARTS   AGE
+nginx-deployment2-54898b49d5-vp6rf   1/1     Running   0          25s
+nginx-deployment2-54898b49d5-vzp6t   1/1     Running   0          25s
+
+#Deleting the application
+pawanbhatta@pawans-MBP CloudComputing % kubectl delete -f nginx-deployment.yaml
+deployment.apps "nginx-deployment2" deleted
+
+#No application running in the cluster
+pawanbhatta@pawans-MBP CloudComputing % kubectl get pods
+No resources found in default namespace.
+
+#Deleting the cluster
+pawanbhatta@pawans-MBP CloudComputing % eksctl delete cluster --name coding-rant-cluster-v1
